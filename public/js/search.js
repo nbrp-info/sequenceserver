34a5fbb6a87c4091aabe9788ce8d4cdf79696ba3
shoji <syoji.kento@nig.ac.jp>
2019-11-28 15:53:08 +0900

    チェックボックスの状態を直接変更した場合、BLAST実行可・不可の制御イベントが発火しないため、全選択ボタンを押下した際の処理にはclick()を使用するよう修正。

--- public/js/search.js
+++ public/js/search.js
@@ -6,12 +6,14 @@ $(".all-button").click(function () {
     $(".db_checkbox").each(function () {
         if ($(this)[0].checked == false) {
             allCheckedFlg = false;
-            $(this)[0].checked = true;
+            $(this)[0].click();
         }
     });
     if (allCheckedFlg == true) {
         $(".db_checkbox").each(function () {
-            $(this)[0].checked = false;
+            if ($(this)[0].checked == true) {
+                $(this)[0].click();
+            }
         });
     }
 });
